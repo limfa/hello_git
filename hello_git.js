3fd1f93631ed5c91cc6c5dc904be24858b1c75f3
limfa <limfa@qq.com>
2014-11-03 13:50:46 +0800

    first commit

---
+++ hello_git.js
@@ -0,0 +1,11 @@
+var note = 'hello_git';
+console.log(note ,'\n');
+note = '# 在GitHub上下载\
+git clone https://github.com/<user_name>/<module_name>';
+console.log(note ,'\n');
+note = 'git add -A';
+console.log(note ,'\n');
+note = 'git commit -m "first commit"';
+console.log(note ,'\n');
+note = 'git push -u origin master';
+console.log(note ,'\n');
