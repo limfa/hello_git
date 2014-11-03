93cfd6d95da67b506b65c510df977f39d74e317c
limfa <limfa@qq.com>
2014-11-03 13:54:15 +0800

    first commin

--- hello_git.js
+++ hello_git.js
@@ -3,9 +3,9 @@ console.log(note ,'\n');
 note = '# 在GitHub上下载\
 git clone https://github.com/<user_name>/<module_name>';
 console.log(note ,'\n');
-note = 'git add -A';
-console.log(note ,'\n');
-note = 'git commit -m "first commit"';
-console.log(note ,'\n');
-note = 'git push -u origin master';
+note = '# 提交到GitHub\
+git add -A\
+git commit -m "first commit"\
+# 如果上一步失败，可根据提示操作\
+git push -u origin master';
 console.log(note ,'\n');
